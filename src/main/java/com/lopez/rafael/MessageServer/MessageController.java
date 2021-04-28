df05b59de82b5a99538d1e4a1b877d25b81e59b0
Rafael Lopez <rafael.lopez@bbd.ca>
2021-04-27 23:39:39 -0400

    Test CI/CD pipeline. Ensure automatic builds work + automatic deployment

--- src/main/java/com/lopez/rafael/MessageServer/MessageController.java
+++ src/main/java/com/lopez/rafael/MessageServer/MessageController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MessageController {
     @GetMapping("/message")
     public String getMessage() {
-        return "Hello from Spring Boot + Maven!";
+        return "Hello from Spring Boot + Maven + Jenkins + AWS!";
     }
 }
